426acee3dc84ff663f5d3f074d635af3173ca6aa
sorathpanzer <sorathpanzer>
2023-03-01 15:13:12 +0000

    up

--- configuration-nvidia.nix
+++ configuration-nvidia.nix
@@ -1,12 +1,22 @@
 { config, pkgs, ... }:
 
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+
 {
 imports = [ ./hardware-configuration.nix ];
 
   boot = {
     supportedFilesystems = [ "zfs" ];
     consoleLogLevel = 0;
-    kernelParams = [ "quiet" "udev.log_level=3" ];
+    kernelParams = [ "quiet" "udev.log_level=3" "nvidia-drm-modeset=1" ]; #nvidia-drm.modeset=1
     initrd.secrets = { "/crypto_keyfile.bin" = null; };
     initrd.verbose = false;
     loader = {
@@ -59,10 +69,15 @@ imports = [ ./hardware-configuration.nix ];
       displayManager.lightdm.enable = false;
       displayManager.startx.enable = true;
       layout = "pt";
-      videoDrivers = [ "intel" ];
+      videoDrivers = [ "nvidia" ];
       deviceSection = ''
         Option "DRI" "3"
         Option "TearFree" "true"
+      '';
+      screenSection = ''
+        Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+        Option         "AllowIndirectGLXProtocol" "off"
+        Option         "TripleBuffer" "on"
       '';
       libinput = {
         enable = true;
@@ -85,6 +100,30 @@ imports = [ ./hardware-configuration.nix ];
     }
   ];
 
+  hardware = {
+    video.hidpi.enable = true;
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      nvidiaPersistenced = true;
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+    };
+  };
+
   users.users.sorath = {
     isNormalUser = true;
     description = "sorath";
@@ -96,7 +135,7 @@ imports = [ ./hardware-configuration.nix ];
      android-tools btrfs-progs dunst feh ffmpeg ffmpegthumbnailer file firefox fzf gcc git gnumake groff i3lock imagemagick
      keepassxc killall lf light lm_sensors libreoffice-still mpv ncdu neovim ntfs3g openssh pandoc picom poppler_utils qemu
      python310Packages.adblock python39Packages.pip python39Packages.six qutebrowser scrot sox stow syncthing tdesktop
-     tig trash-cli udiskie ueberzug unzip usbutils w3m xclip xdg-user-dirs xdotool xorg.xf86videointel xorg.xinput xorg.xrandr jq
+     tig trash-cli udiskie ueberzug unzip usbutils w3m xclip xdg-user-dirs xdotool xorg.xf86videointel xorg.xinput xorg.xrandr jq mesa-demos nvidia-offload
      xorg.xrdb xorg.xset youtube-dl zathura pulseaudio dmenu signal-desktop bzip2 foot wayland-protocols hyprpaper mpvpaper waybar river dwl ydotool
    (pkgs.st.overrideAttrs (oldAttrs: {
       name = "st";
@@ -113,7 +152,7 @@ imports = [ ./hardware-configuration.nix ];
   ];
 
   nixpkgs = {
-    #config.allowUnfree = true;
+    config.allowUnfree = true;
     overlays = [
       (self: super: {
         dwm = super.dwm.overrideAttrs (old: { src = /home/sorath/.config/suckless/dwm-6.4 ;});
@@ -144,7 +183,7 @@ imports = [ ./hardware-configuration.nix ];
     settings.auto-optimise-store = true;
     gc.automatic = true;
     gc.dates = "weekly";
-    gc.options = "--delete-older-than 7d";
+    gc.options = "--delete-older-than 30d";
     extraOptions = "experimental-features = nix-command flakes";
     package = pkgs.nixFlakes;
   };
