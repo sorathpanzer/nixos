1e0fbb2ba5cff712de7ff3c88dba479b47098715
sorathpanzer <sorathpanzer>
2023-03-03 06:06:57 +0000

    up

--- configuration-nvidia.nix
+++ configuration-nvidia.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 let
   nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
@@ -16,7 +16,7 @@ imports = [ ./hardware-configuration.nix ];
   boot = {
     supportedFilesystems = [ "zfs" ];
     consoleLogLevel = 0;
-    kernelParams = [ "quiet" "udev.log_level=3" "nvidia-drm-modeset=1" ]; #nvidia-drm.modeset=1
+    kernelParams = [ "quiet" "udev.log_level=3" "i915" ];
     initrd.secrets = { "/crypto_keyfile.bin" = null; };
     initrd.verbose = false;
     loader = {
@@ -50,8 +50,37 @@ imports = [ ./hardware-configuration.nix ];
     #wg-quick.interfaces.wg0.autostart;
   };
 
+  environment.variables = {
+    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+  };
+
+  hardware = {
+    cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nvidia = {
+      modesetting.enable = lib.mkDefault true;
+      powerManagement.enable = lib.mkDefault true;
+      nvidiaPersistenced = true;
+
+      prime = {
+        offload.enable = lib.mkOverride 990 true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+    opengl.extraPackages = with pkgs; [
+      vaapiIntel
+      libvdpau-va-gl
+      intel-media-driver
+    ];
+  };
+
   services = {
     getty.autologinUser = "sorath";
+    tlp.enable = lib.mkDefault ((lib.versionOlder (lib.versions.majorMinor lib.version) "22.11")
+                                       || !config.services.power-profiles-daemon.enable);
+    thermald.enable = lib.mkDefault true;
+    hdapsd.enable = lib.mkDefault true;
     syncthing = {
       enable = true;
       user = "sorath";
@@ -69,7 +98,7 @@ imports = [ ./hardware-configuration.nix ];
       displayManager.lightdm.enable = false;
       displayManager.startx.enable = true;
       layout = "pt";
-      videoDrivers = [ "nvidia" ];
+      videoDrivers = lib.mkDefault [ "nvidia" ];
       deviceSection = ''
         Option "DRI" "3"
         Option "TearFree" "true"
@@ -100,30 +129,6 @@ imports = [ ./hardware-configuration.nix ];
     }
   ];
 
-  hardware = {
-    video.hidpi.enable = true;
-    nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-      modesetting.enable = true;
-      nvidiaPersistenced = true;
-      prime = {
-        offload.enable = true;
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-    opengl = {
-      enable = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
-  };
-
   users.users.sorath = {
     isNormalUser = true;
     description = "sorath";
@@ -134,8 +139,8 @@ imports = [ ./hardware-configuration.nix ];
   environment.systemPackages = with pkgs; [
      android-tools btrfs-progs dunst feh ffmpeg ffmpegthumbnailer file firefox fzf gcc git gnumake groff i3lock imagemagick
      keepassxc killall lf light lm_sensors libreoffice-still mpv ncdu neovim ntfs3g openssh pandoc picom poppler_utils qemu
-     python310Packages.adblock python39Packages.pip python39Packages.six qutebrowser scrot sox stow syncthing tdesktop
-     tig trash-cli udiskie ueberzug unzip usbutils w3m xclip xdg-user-dirs xdotool xorg.xf86videointel xorg.xinput xorg.xrandr jq mesa-demos nvidia-offload
+     python310Packages.adblock python39Packages.pip python39Packages.six qutebrowser scrot sox stow syncthing tdesktop vaapiVdpau
+     tig trash-cli udiskie ueberzug unzip usbutils w3m xclip xdg-user-dirs xdotool xorg.xf86videointel xorg.xinput xorg.xrandr jq nvidia-offload mesa-demos
      xorg.xrdb xorg.xset youtube-dl zathura pulseaudio dmenu signal-desktop bzip2 foot wayland-protocols hyprpaper mpvpaper waybar river dwl ydotool
    (pkgs.st.overrideAttrs (oldAttrs: {
       name = "st";
@@ -183,7 +188,7 @@ imports = [ ./hardware-configuration.nix ];
     settings.auto-optimise-store = true;
     gc.automatic = true;
     gc.dates = "weekly";
-    gc.options = "--delete-older-than 30d";
+    gc.options = "--delete-older-than 7d";
     extraOptions = "experimental-features = nix-command flakes";
     package = pkgs.nixFlakes;
   };
